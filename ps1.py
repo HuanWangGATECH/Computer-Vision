015376e0e0ddeb208710b438f551a779a18b6152
Chuqiao Dong <chd415@lehigh.edu>
2019-02-17 23:09:18 -0500

    finish with project 1

---
+++ ps1.py
@@ -0,0 +1,254 @@
+import math
+import numpy as np
+import cv2
+import sys
+
+# # Implement the functions below.
+
+
+def extract_red(image):
+    """ Returns the red channel of the input image. It is highly recommended to make a copy of the
+    input image in order to avoid modifying the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): Input RGB (BGR in OpenCV) image.
+
+    Returns:
+        numpy.array: Output 2D array containing the red channel.
+    """
+    temp_image = np.copy(image)
+    temp_red = temp_image[:,:,2]
+    return temp_red
+
+
+
+def extract_green(image):
+    """ Returns the green channel of the input image. It is highly recommended to make a copy of the
+    input image in order to avoid modifying the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): Input RGB (BGR in OpenCV) image.
+
+    Returns:
+        numpy.array: Output 2D array containing the green channel.
+    """
+    temp_image = np.copy(image)
+    temp_green = temp_image[:,:,1]
+    return temp_green
+
+
+def extract_blue(image):
+    """ Returns the blue channel of the input image. It is highly recommended to make a copy of the
+    input image in order to avoid modifying the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): Input RGB (BGR in OpenCV) image.
+
+    Returns:
+        numpy.array: Output 2D array containing the blue channel.
+    """
+    temp_image = np.copy(image)
+    temp_blue = temp_image[:,:,0]
+    return temp_blue
+
+
+def swap_green_blue(image):
+    """ Returns an image with the green and blue channels of the input image swapped. It is highly
+    recommended to make a copy of the input image in order to avoid modifying the original array.
+    You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): Input RGB (BGR in OpenCV) image.
+
+    Returns:
+        numpy.array: Output 3D array with the green and blue channels swapped.
+    """
+    temp_swap = np.copy(image)
+    temp_swap[:,:,1] = image[:,:,0]
+    temp_swap[:,:,0] = image[:,:,1]
+    return temp_swap
+
+
+def copy_paste_middle(src, dst, shape):
+    """ Copies the middle region of size shape from src to the middle of dst. It is
+    highly recommended to make a copy of the input image in order to avoid modifying the
+    original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+        Note: Assumes that src and dst are monochrome images, i.e. 2d arrays.
+
+        Note: Where 'middle' is ambiguous because of any difference in the oddness
+        or evenness of the size of the copied region and the image size, the function
+        rounds downwards.  E.g. in copying a shape = (1,1) from a src image of size (2,2)
+        into an dst image of size (3,3), the function copies the range [0:1,0:1] of
+        the src into the range [1:2,1:2] of the dst.
+
+    Args:
+        src (numpy.array): 2D array where the rectangular shape will be copied from.
+        dst (numpy.array): 2D array where the rectangular shape will be copied to.
+        shape (tuple): Tuple containing the height (int) and width (int) of the section to be
+                       copied.
+
+    Returns:
+        numpy.array: Output monochrome image (2D array)
+    """
+    temp_copy = np.copy(dst)
+    (w, h) = src.shape
+    wl = int(w / 2. - shape[0] / 2.)
+    wr = wl + shape[0]
+    hd = int(h / 2. - shape[1] / 2.)
+    hu = hd + shape[1]
+    (a,b) = dst.shape
+    al = int(a / 2. - shape[0] / 2.)
+    ar = al + shape[0]
+    bd = int(b / 2. - shape[1] / 2.)
+    bu = bd + shape[1]
+    temp_copy[al:ar,bd:bu] = src[wl:wr,hd:hu]
+    return temp_copy
+
+
+def image_stats(image):
+    """ Returns the tuple (min,max,mean,stddev) of statistics for the input monochrome image.
+    In order to become more familiar with Numpy, you should look for pre-defined functions
+    that do these operations i.e. numpy.min.
+
+    It is highly recommended to make a copy of the input image in order to avoid modifying
+    the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): Input 2D image.
+
+    Returns:
+        tuple: Four-element tuple containing:
+               min (float): Input array minimum value.
+               max (float): Input array maximum value.
+               mean (float): Input array mean / average value.
+               stddev (float): Input array standard deviation.
+    """
+    temp_img = np.copy(image)
+    minval = np.min(temp_img) * 1.
+    maxval = np.max(temp_img) * 1.
+    meanval = np.mean(temp_img)
+    stdval = np.std(temp_img)
+    return (minval,maxval,meanval,stdval)
+
+
+def center_and_normalize(image, scale):
+    """ Returns an image with the same mean as the original but with values scaled about the
+    mean so as to have a standard deviation of "scale".
+
+    Note: This function makes no defense against the creation
+    of out-of-range pixel values.  Consider converting the input image to
+    a float64 type before passing in an image.
+
+    It is highly recommended to make a copy of the input image in order to avoid modifying
+    the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): Input 2D image.
+        scale (int or float): scale factor.
+
+    Returns:
+        numpy.array: Output 2D image.
+    """
+    temp_img = np.copy(image)
+    meanval = np.mean(temp_img)
+    stdval = np.std(temp_img)
+    temp_out = (temp_img - meanval) / stdval * scale + meanval
+    return temp_out
+
+
+def shift_image_left(image, shift):
+    """ Outputs the input monochrome image shifted shift pixels to the left.
+
+    The returned image has the same shape as the original with
+    the BORDER_REPLICATE rule to fill-in missing values.  See
+
+    http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/copyMakeBorder/copyMakeBorder.html?highlight=copy
+
+    for further explanation.
+
+    It is highly recommended to make a copy of the input image in order to avoid modifying
+    the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): Input 2D image.
+        shift (int): Displacement value representing the number of pixels to shift the input image.
+            This parameter may be 0 representing zero displacement.
+
+    Returns:
+        numpy.array: Output shifted 2D image.
+    """
+    (h, w) = image.shape
+    #print(h,w)
+    temp_shift = np.copy(image[:,0:w-shift])
+    temp_shift[:,0:w-shift] = image[:,shift:]
+    (a, b) = temp_shift.shape
+    #print(a,b)
+    temp_shift = cv2.copyMakeBorder(temp_shift, top = 0, bottom = 0, left = 0, right = shift, borderType = cv2.BORDER_REPLICATE)
+    (a, b) = temp_shift.shape
+    #print(a,b)
+    return temp_shift
+
+
+def difference_image(img1, img2):
+    """ Returns the difference between the two input images (img1 - img2). The resulting array must be normalized
+    and scaled to fit [0, 255].
+
+    It is highly recommended to make a copy of the input image in order to avoid modifying
+    the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        img1 (numpy.array): Input 2D image.
+        img2 (numpy.array): Input 2D image.
+
+    Returns:
+        numpy.array: Output 2D image containing the result of subtracting img2 from img1.
+    """
+    temp_img1 = np.copy(img1)
+    temp_img2 = np.copy(img2)
+    temp_out = temp_img1 - temp_img2
+    temp_out = cv2.normalize(temp_out, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
+
+    return temp_out
+
+
+def add_noise(image, channel, sigma):
+    """ Returns a copy of the input color image with Gaussian noise added to
+    channel (0-2). The Gaussian noise mean must be zero. The parameter sigma
+    controls the standard deviation of the noise.
+
+    The returned array values must not be clipped or normalized and scaled. This means that
+    there could be values that are not in [0, 255].
+
+    Note: This function makes no defense against the creation
+    of out-of-range pixel values.  Consider converting the input image to
+    a float64 type before passing in an image.
+
+    It is highly recommended to make a copy of the input image in order to avoid modifying
+    the original array. You can do this by calling:
+    temp_image = np.copy(image)
+
+    Args:
+        image (numpy.array): input RGB (BGR in OpenCV) image.
+        channel (int): Channel index value.
+        sigma (float): Gaussian noise standard deviation.
+
+    Returns:
+        numpy.array: Output 3D array containing the result of adding Gaussian noise to the
+            specified channel.
+    """
+    mean = 0.0
+    row, col, ch = image.shape
+    gauss = np.random.normal(mean, sigma, (row, col))
+    temp_gauss = np.copy(image)
+    temp_gauss[:,:,channel] = temp_gauss[:,:,channel] + gauss
+    return temp_gauss
